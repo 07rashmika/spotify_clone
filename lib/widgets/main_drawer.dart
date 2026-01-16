9ded6df8029c776120a53c788ce2bbe38bf4e541
07rashmika <rashmikadesilva07@gmail.com>
2026-01-17 00:47:03 +0530

    -header of the side drawer

--- lib/widgets/main_drawer.dart
+++ lib/widgets/main_drawer.dart
@@ -1,9 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:spotify_clone/models/colors.dart';
 
 class MainDrawer extends StatelessWidget {
-  const MainDrawer({super.key, required this.drawerIcon});
+  const MainDrawer({
+    super.key,
+    required this.drawerIcon,
+    required this.userName,
+  });
 
   final Widget drawerIcon;
+  final String userName;
 
   @override
   Widget build(BuildContext context) {
@@ -23,6 +29,24 @@ class MainDrawer extends StatelessWidget {
                       borderRadius: BorderRadius.circular(50),
                     ),
                     child: drawerIcon,
+                  ),
+                  Column(
+                    children: [
+                      Text(
+                        userName,
+                        style: TextStyle(
+                          fontSize: 28,
+                          color: SpotifyColors.white,
+                        ),
+                      ),
+                      Text(
+                        'View Profile',
+                        style: TextStyle(
+                          fontSize: 16,
+                          color: SpotifyColors.mediumGrey,
+                        ),
+                      ),
+                    ],
                   ),
                 ],
               ),
